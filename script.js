9d3ae3a0a25b0e1399a2ba1cd2d119aee7216fe2
Amresh Maurya <amresh2478@gmail.com>
2023-04-19 12:14:52 +0530

    half of todo is done

---
+++ script.js
@@ -0,0 +1,34 @@
+const ul = document.querySelector('ul');
+const input = document.getElementById('item');
+// Global Array 
+let itemsArray = JSON.parse(localStorage.getItem('todoList')) || [];
+
+// showing data
+itemsArray.forEach(addTask);
+function addTask(text,id){
+  const li = document.createElement('li')
+  li.textContent = `${id}. ${text}`;
+  ul.appendChild(li);
+}
+
+
+// add task function
+function add(){
+  // Current object
+const currentObject={
+  title:input.value,
+  id: itemsArray.length>0 ? itemsArray[itemsArray.length].id+1 : 1
+}
+
+
+  itemsArray.push(currentObject);
+  localStorage.setItem('todoList', JSON.stringify(itemsArray));
+  addTask(currentObject.title,currentObject.id);
+  input.value = '';
+}
+
+function del(){
+  localStorage.clear();
+  ul.innerHTML = '';
+  itemsArray = [];
+}
